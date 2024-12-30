d2b54a84dc36e52a735964ce3566a9c99d3eb577
Rasmus P <djrasmusp@users.noreply.github.com>
2024-12-30 14:34:17 +0100

    feature(12): New branch command
    ENV: prod

---
+++ src/commands/createBranch.js
@@ -0,0 +1,41 @@
+import {select, input} from '@inquirer/prompts';
+import {COMMIT_TYPES} from '../utils/constants.js'
+import {logError} from "../utils/logger.js";
+import {newBranch} from "../utils/gitUtils.js";
+import {kebabCase} from "../utils/utils.js";
+
+export async function createBranch() {
+    try {
+        const answers = {
+            scope: await select({
+                message: 'Select branch scope',
+                choices: COMMIT_TYPES.sort().map(item => {
+                    return {
+                        name: item,
+                        value: item
+                    }
+                })
+            }),
+            id: await input({
+                message: 'Task id',
+            }),
+            title: await input({
+                    message: 'Commit title',
+                    required: true,
+                }
+            )
+        }
+
+        const title = kebabCase(answers.title, !(answers.id.length == 0))
+        const name = `${answers.scope}/${answers.id}${title}`
+
+        await newBranch(name)
+    } catch (error) {
+        if (error instanceof Error && error.name === 'ExitPromptError') {
+            console.log('👋 until next time!');
+            return
+        }
+
+        logError(error)
+    }
+}
