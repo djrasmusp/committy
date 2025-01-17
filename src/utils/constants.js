c339af0b85bc21b9a7e84ffbc5af56a2bc171453
Rasmus P <djrasmusp@users.noreply.github.com>
2025-01-17 11:12:18 +0100

    Update constants.js
    Add more commit types

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,2 @@
-export const COMMIT_TYPES = ['feature', 'bug', 'chore', 'release', 'fix', 'docs', 'refactor']
+export const COMMIT_TYPES = ['feature', 'bug', 'chore', 'release', 'fix', 'docs', 'refactor', 'poc', 'change']
 export const ENVIROMENTS = ['dev', 'test', 'preprod', 'prod']
